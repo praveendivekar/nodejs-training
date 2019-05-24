08eb2239b9ca2cd594d90030d987f336fb503516
praveen divekar <pdivekar>
2019-05-24 23:18:44 +0530

    Middlwaer

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 // Simple express server with PORT
 import express from 'express';
-import logger from './middlewares/application-middleware';
-import appRouter from './routes/router';
+import {logger, addTime} from './middlewares/application-middleware';
+import {itemRouter} from './middlewares/router-middleware';
+import { errorHandler } from './middlewares/error-middleware';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 3000;
 
-app.use(logger);
+app.use('/api/item', itemRouter);
+app.get('/api/admin', errorHandler);
 
-app.use('/', appRouter);
+// Pass the middlwares as an array
+app.use('/api/application', [bodyParser, logger, addTime], (req, res) => {
+  res.send({messge: 'Hello!'});
+});
 
 // Listen the application with specified port
 app.listen(PORT, () => {
--- src/middlewares/application-middleware.js
+++ src/middlewares/application-middleware.js
@@ -1,7 +1,15 @@
 // simple application level middlware for logging
 const logger = (req, res, next) => {
-  console.log(`In application logger`);
+  console.log(`In application logger ${req.time}`);
   next();
 }
 
-export default logger;
+const addTime = (req, res, next) => {
+  req.time = new Date(Date.now());
+  next();
+}
+
+export {
+  logger,
+  addTime
+};
