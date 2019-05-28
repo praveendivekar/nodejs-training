67b99d9af0429a7aad95ce196ef08dbaddb25367
praveen divekar <pdivekar>
2019-05-28 16:56:56 +0530

    Middlware solution and workspace

--- src/app.js
+++ src/app.js
@@ -1,16 +1,8 @@
-// Simple express server with PORT
 import express from 'express';
-import logger from './middlewares/application-middleware';
-import appRouter from './routes/router';
 
 const app = express();
 const PORT = 3000;
 
-app.use(logger);
-
-app.use('/', appRouter);
-
-// Listen the application with specified port
 app.listen(PORT, () => {
-  console.log(`Server is at ${PORT}`);
+  console.log(`App is running on ${PORT}`);
 });
---
+++ src/solution/app.js
@@ -0,0 +1,17 @@
+// TODO:- Write a simple logger middlware and hook into the app js
+import express from 'express';
+import logger from './middlewares/logger';
+
+const app = express();
+const PORT = 3000;
+
+app.use(logger);
+
+app.get('/', (req, res) => {
+  res.send({message: 'Hello! from app'});
+})
+
+// Listen the application with specified port
+app.listen(PORT, () => {
+  console.log(`Server is at ${PORT}`);
+});
